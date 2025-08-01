fa5581bed825677fe6c6a4f10b5b919d84c36902
MOHAMED AIT TAJANTE <mhdjant607@gmail.com>
2025-08-01 20:59:32 +0100

    mari up1

--- srcs/requirements/mariadb/tools/set-up-dataBase.sh
+++ srcs/requirements/mariadb/tools/set-up-dataBase.sh
@@ -5,15 +5,23 @@ source /.env
 set +a
 
 echo "starting mariadb server .."
-mysqld_safe &
+
+service mariadb start
 
 until mysqladmin ping --silent; do
     echo "waiting for mariadb .."
 done
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \'${MYSQL_DATABASE}\';"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+
 echo "wordpress DataBase has been created!"
-mysql -e "CREATE USER IF NOT EXISTS \'${MYSQL_USER}\'@'localhost' IDENTIFIED BY \'${MYSQL_PASSWORD}\';"
-mysql -e "GRANTs ALL PRIVILEGES ON \'${MYSQL_DATABASE}\'.* TO \'${MYSQL_USER}\'@'%' IDENTIFIED BY \'${MYSQL_PASSWORD}\';"
+
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
 echo "new user '${MYSQL_USER}' created with All privileges!"
-mysql -e "FLUSH ALL PRIVILEGES"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+
