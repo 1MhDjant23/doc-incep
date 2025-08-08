bcd2385ff2a6bc1aa82d4c8db4a855bc33bccf0c
MOHAMED AIT TAJANTE <mhdjant607@gmail.com>
2025-08-08 11:29:03 +0100

    nginx-conf

--- srcs/requirements/wordpress/conf/wordPress-config.sh
+++ srcs/requirements/wordpress/conf/wordPress-config.sh
@@ -10,7 +10,10 @@ DIR="/var/www/wordpress"
 
 
 #   start PHP-FPM ON FOREGROUND #
-wp user create mohamed mohamed@gmail.com --role=administrator --path=/var/www/wordpress --user_pass=1234 --allow-root
-wp --info --allow-root --path=/var/www/wordpress
+wp core install --url="http://localhost" --title="mait-taj Site" --admin_user="admin" --admin_password="1234" --admin_email="admin@example.com" --path=/var/www/wordpress --allow-root
+
+
+# wp user create mohamed mohamed@gmail.com --role=administrator --path=/var/www/wordpress --user_pass=1234 --allow-root
+# wp --info --allow-root --path=/var/www/wordpress
 
 php-fpm8.4 -F
